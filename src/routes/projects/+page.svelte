acdcd3b5712e5ddae7fb3060fd7f22e8a79af782
Siwoo Jeon <me@migan.co.kr>
2025-02-13 13:47:47 +0900

    feat: Add carbonium

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,8 +1,8 @@
 <script>
   import doremiPhoto from '$lib/images/doremi.png'
-  import discommandPhoto from '$lib/images/discommand.png'
+  // import discommandPhoto from '$lib/images/discommand.png'
 
-  const PROJECT_MOVE = '해당 프로젝트로 이동하기'
+  // const PROJECT_MOVE = '해당 프로젝트로 이동하기'
 </script>
 
 <main>
@@ -10,23 +10,24 @@
     <div
       class="group w-[320px] h-[160px] md:w-[640px] md:h-[320px] overflow-hidden m-auto"
     >
-      <a href="https://github.com/Migan-Studio/Doremi" target="_blank">
-        <img
-          src={doremiPhoto}
-          alt="Doremi"
-          class="w-[100%] h-[100%] object-cover rounded-3xl duration-350 group-hover:brightness-50"
-        />
-        <span
-          class="text-(--text-color) top-[49%] left-[46%] opacity-0 absolute duration-350 group-hover:opacity-100"
-          >{PROJECT_MOVE}</span
-        >
-      </a>
+      <!-- <a href="https://github.com/Migan-Studio/Doremi" target="_blank"> -->
+      <!-- 사진 변경 예정 -->
+      <img
+        src={doremiPhoto}
+        alt="Carbonium (Preparing)/카보늄 (준비 중)"
+        class="w-[100%] h-[100%] object-cover rounded-3xl duration-350 group-hover:brightness-50"
+      />
+      <span
+        class="text-(--text-color) top-[49%] left-[46%] opacity-0 absolute duration-350 group-hover:opacity-100"
+        >공개 예정</span
+      >
+      <!-- </a> -->
     </div>
     <p class="text-base md:text-lg">
-      Doremi는 Migan이 만들고, 운영하는 디코봇이에요.
+      Carbonium은 Migan이 참여하는 오픈소스 마인크래프트 실행기에요. (공개 예정)
     </p>
   </div>
-  <div class="mt-11 text-center relative">
+  <!-- <div class="mt-11 text-center relative">
     <div
       class="group w-[320px] h-[160px] md:w-[640px] md:h-[320px] overflow-hidden m-auto"
     >
@@ -45,5 +46,5 @@
     <p class="text-base md:text-lg">
       discommand는 Migan이 만드는 discord.js에 쓸 수 있는 라이브러리에요.
     </p>
-  </div>
+  </div> -->
 </main>
