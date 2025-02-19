bd6b959faf5d2a946f16341ef50c47e415b8e95d
Siwoo Jeon <me@migan.co.kr>
2025-02-19 14:39:15 +0900

    feat: Add my birth

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,25 @@
+<script lang="ts">
+  import myProfileBg from '$lib/images/myprofile-bg.png'
+
+  let qwer: number
+  const birth = new Date('2010-01-31T04:28:00+09:00').getTime()
+  qwer = Date.now() - birth
+  setInterval(() => {
+    qwer = Date.now() - birth
+  })
+</script>
+
 <main>
-  <h1 class="text-center mt-50">안녕하세요, 저는 Migan이에요!</h1>
+  <div class="text-center">
+    <img
+      src={myProfileBg}
+      alt="Migan's profile/미간의 프로필"
+      class="rounded-[50%] m-auto mt-50"
+      width="150"
+    />
+    <h1 class="text-5xl">Migan</h1>
+    <h2 class="mt-3">태어난지 {new Intl.NumberFormat().format(qwer)}ms</h2>
+  </div>
   <ul class="list-none flex items-center justify-center">
     <li class="px-7 py-8 text-xl">
       <a href="https://github.com/Migan178" target="_blank">깃허브로 가기</a>
