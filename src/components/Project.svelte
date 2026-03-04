6d7c73f3e8f2f8a89c34f9a84b5b1d148480952d
Siwoo Jeon <me@migan.co.kr>
2026-03-04 20:43:04 +0900

    feat: add miganart in projects

--- src/components/Project.svelte
+++ src/components/Project.svelte
@@ -2,25 +2,26 @@
 	export let image: string;
 	export let description: string;
 	export let imageAlt: string;
+	export let href: string;
 </script>
 
-<div class="text-center relative z-10">
+<div class="relative z-10 text-center">
 	<div
-		class="group w-[320px] h-[160px] md:w-[640px] md:h-[320px] overflow-hidden m-auto"
+		class="group m-auto h-[160px] w-[320px] overflow-hidden md:h-[320px] md:w-[640px]"
 	>
-		<a href="https://github.com/Muffin-laboratory/goMuffin" target="_blank">
+		<a {href} target="_blank">
 			<img
 				src={image}
 				alt={imageAlt}
-				class="w-[100%] h-[100%] object-cover rounded-3xl duration-350 group-hover:brightness-50"
+				class="h-[100%] w-[100%] rounded-3xl object-cover duration-350 group-hover:brightness-50"
 			/>
 			<span
-				class="text-(--default-text-color) top-[50%] left-[50%] translate-[-50%] opacity-0 absolute duration-350 group-hover:opacity-100"
+				class="absolute top-[50%] left-[50%] translate-[-50%] text-(--default-text-color) opacity-0 duration-350 group-hover:opacity-100"
 				>해당 프로젝트로 이동하기</span
 			>
 		</a>
 	</div>
-	<p class="text-base md:text-lg mt-2">
+	<p class="mt-2 text-base md:text-lg">
 		{description}
 	</p>
 </div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import noPicturePhoto from "$lib/images/nopicture.png";
+	import miganart from "$lib/images/miganart-gh.png";
 	import Project from "../../components/Project.svelte";
 </script>
 
@@ -9,13 +10,15 @@
 			image={noPicturePhoto}
 			imageAlt="사진 없음/None"
 			description="머핀봇은 Migan이 만드는 내수용 봇이에요."
+			href="https://github.com/Muffin-laboratory/goMuffin.git"
 		/>
 	</div>
 	<div class="mt-11 pb-25">
 		<Project
-			image={noPicturePhoto}
-			imageAlt="사진 없음/None"
-			description="MCRiderBOT은 Migan이 참여하는 디스코드 봇으로, 마크라이더 공식 디스코드에서 기록 관리를 위한 봇이에요."
+			image={miganart}
+			imageAlt="미간라트"
+			description="Migan이 만든 간단한 만다라트 작성 앱이에요."
+			href="https://mandalart.migan.co.kr"
 		/>
 	</div>
 </main>
