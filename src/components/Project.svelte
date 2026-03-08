54f2b4d5f33ebbdcf8fafb228965bb945510a82a
Siwoo Jeon <me@migan.co.kr>
2026-03-08 11:00:10 +0900

    fix: tailwind css style tag

--- src/components/Project.svelte
+++ src/components/Project.svelte
@@ -13,17 +13,15 @@
 </script>
 
 <div class="relative z-10 text-center">
-	<div
-		class="group m-auto h-[160px] w-[320px] overflow-hidden md:h-[320px] md:w-[640px]"
-	>
+	<div class="group m-auto h-40 w-[320px] overflow-hidden md:h-80 md:w-160">
 		<a {href} target="_blank">
 			<img
 				src={image}
 				alt={imageAlt}
-				class="h-[100%] w-[100%] rounded-3xl object-cover duration-350 group-hover:brightness-50"
+				class="h-full w-full rounded-3xl object-cover duration-350 group-hover:brightness-50"
 			/>
 			<span
-				class="absolute top-[50%] left-[50%] translate-[-50%] text-(--default-text-color) opacity-0 duration-350 group-hover:opacity-100"
+				class="absolute top-1/2 left-1/2 -translate-1/2 text-(--default-text-color) opacity-0 duration-350 group-hover:opacity-100"
 				>해당 프로젝트로 이동하기</span
 			>
 		</a>
