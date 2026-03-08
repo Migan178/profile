aff7232acd1616318f2dd236e3fd7ba23280cb3e
Siwoo Jeon <me@migan.co.kr>
2026-03-08 21:29:20 +0900

    refactor: use custom modal instead browser default alert in your lover

---
+++ src/components/layouts/ModalNotification.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	let { msg, closeModal }: { msg: string; closeModal: () => void } = $props();
+</script>
+
+<div
+	class="fixed top-0 left-0 z-100 flex h-full w-full items-center justify-center bg-black/50"
+>
+	<div class="rounded-4xl bg-(--bg-color) p-8 shadow-xl">
+		<button
+			onclick={closeModal}
+			class="text-(--text-color) duration-250 hover:cursor-pointer hover:text-(--hover-color)"
+			aria-label="닫기 버튼"
+		>
+			<i class="bi bi-x-lg"></i>
+		</button>
+		<p>{msg}</p>
+	</div>
+</div>
--- src/routes/your-lover/+page.svelte
+++ src/routes/your-lover/+page.svelte
@@ -1,10 +1,18 @@
 <script>
-	function returnYesOrNo() {
-		const a = Math.floor(Math.random() * 2 ** 53);
-		console.log(a);
+	import ModalNotification from "../../components/layouts/ModalNotification.svelte";
 
-		if (a === 1) return alert("응");
-		else return alert("아니");
+	let msg = $state("");
+
+	function returnYesOrNo() {
+		const isLover = Math.floor(Math.random() * 2 ** 53);
+		console.log(isLover);
+
+		if (isLover === 1)
+			msg =
+				"당신은 이제부터 이 웹사이트의 연인이에요! 행복한 시간 보내세요 :)";
+		else
+			msg =
+				"아쉽게도 해당 웹사이트의 여친이 되지 못 했어요 :( 다음을 노려봐요.";
 	}
 </script>
 
@@ -13,10 +21,14 @@
 		<h1>이 웹사이트의 애인이 되어보세요!</h1>
 		<h2>확률은 2의 53승분의 1입니다.</h2>
 		<button
-			class="rounded-xl bg-(--bg-color) border-2 border-solid border-blue-950 w-40 h-8 text-(--text-color) hover:text-white shadow-2xl shadow-blue-950 hover:cursor-pointer hover:bg-blue-950 duration-250"
-			on:click={returnYesOrNo}
+			class="h-8 w-40 rounded-xl border-2 border-solid border-blue-950 bg-(--bg-color) text-(--text-color) shadow-2xl shadow-blue-950 duration-250 hover:cursor-pointer hover:bg-blue-950 hover:text-white"
+			onclick={returnYesOrNo}
 		>
 			내가 너의 남친이니?
 		</button>
 	</div>
 </main>
+
+{#if msg}
+	<ModalNotification {msg} closeModal={() => (msg = "")} />
+{/if}
