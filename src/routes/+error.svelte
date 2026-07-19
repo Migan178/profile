9f55e5905987db4edacaaee69842f133c3d151f6
Siwoo Jeon <me@miganbox.com>
2026-07-19 23:27:58 +0900

    fix: error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,11 +1,10 @@
 <script>
 	import { resolve } from "$app/paths";
-	import { page } from "$app/state";
 </script>
 
 <main>
-	<div class="mt-50 text-center text-3xl">
-		<h1>{page.status} {page.error?.message}</h1>
+	<div class="text-center text-3xl">
+		<h1>404 Not Found :(</h1>
 		<a href={resolve("/")}>홈으로 가기</a>
 	</div>
 </main>
